1a2641cdf03172e86cac9fb89b0b8ad6be340e74
delphinus <me@delphinus.dev>
2020-08-16 21:49:34 +0900

    Rewrite sources with using common vimscript funcs

--- rplugin/python3/denite/source/memo.py
+++ rplugin/python3/denite/source/memo.py
@@ -4,19 +4,15 @@
 # License: MIT license
 # ============================================================================
 
-from subprocess import CalledProcessError
-import sys
-from pathlib import Path
-from typing import Optional, cast
+from platform import system
+from typing import Dict, Optional, cast
+from unicodedata import east_asian_width, normalize
 
-sys.path.append(str(Path(__file__).parent.parent.parent.resolve()))
-
-from vim_denite_memo.command import Memo
-from vim_denite_memo.string import stdwidthpart
 from denite import process
 from denite.source.base import Base
 from denite.util import Candidate, Candidates, UserContext, Nvim
 
+EAST_ASIAN_WIDTH: Dict[str, str] = {}
 HIGHLIGHT_SYNTAX = [
     {"name": "Prefix", "link": "String", "re": r"\[new title\]"},
     {"name": "File", "link": "String", "re": r"\v.*( : )@="},
@@ -34,17 +30,22 @@ class Source(Base):
 
     def on_init(self, context: UserContext) -> None:
         self.__set_proc(context, None)
-        self.__set_memo(context, Memo())
 
     def gather_candidates(self, context: UserContext) -> Candidates:
+        exe = self.vim.call("denite#memo#executable")
+        if not exe:
+            return []
+
         if context["args"] and context["args"][0] == "new":
             return self._is_new(context)
 
         if self.__proc(context):
             return self.__async_gather_candidates(context, context["async_timeout"])
 
-        proc = self.__memo(context).proc(
-            context, "list", "--format", "{{.Fullpath}}\t{{.File}}\t{{.Title}}"
+        proc = process.Process(
+            [exe, "list", "--format", "{{.Fullpath}}\t{{.File}}\t{{.Title}}"],
+            context,
+            context["path"],
         )
         self.__set_proc(context, proc)
         return self.__async_gather_candidates(context, context["async_timeout"])
@@ -65,7 +66,9 @@ class Source(Base):
 
         def make_candidates(row: str) -> Candidate:
             fullpath, filename, title = row.split("\t", 2)
-            cut = stdwidthpart(filename, self.vars["column"], self.vars["ambiwidth"])
+            cut = self.__stdwidthpart(
+                filename, self.vars["column"], self.vars["ambiwidth"]
+            )
             return {
                 "word": filename,
                 "abbr": f"{cut} : {title}",
@@ -76,12 +79,8 @@ class Source(Base):
 
     def _is_new(self, context: UserContext) -> Candidates:
         if "memo_dir" not in self.vars or not self.vars["memo_dir"]:
-            try:
-                self.vars["memo_dir"] = self.__memo(context).get_memo_dir()
-            except CalledProcessError as err:
-                self.error_message(
-                    context, "command returned invalid response: " + str(err)
-                )
+            self.vars["memo_dir"] = self.vim.call("denite#memo#get_dir")
+            if not self.vars["memo_dir"]:
                 return []
 
         context["is_interactive"] = True
@@ -111,14 +110,42 @@ class Source(Base):
                 )
             )
 
-    def __memo(self, context: UserContext) -> Memo:
-        return cast(Memo, context["__memo"])
-
-    def __set_memo(self, context: UserContext, memo: Memo) -> None:
-        context["__memo"] = memo
-
     def __proc(self, context: UserContext) -> Optional[process.Process]:
         return cast(process.Process, context["__proc"]) if context["__proc"] else None
 
     def __set_proc(self, context: UserContext, proc: Optional[process.Process]) -> None:
         context["__proc"] = proc
+
+    def __stdwidthpart(self, string: str, col: int, ambiwidth: str) -> str:
+        # normalize string for filenames in macOS
+        target = normalize("NFC", string) if system() == "Darwin" else string
+        char_lengths = [(x, self.__char_width(x, ambiwidth)) for x in target]
+        sum_len = sum(x for (_, x) in char_lengths)
+        if sum_len < col:
+            return target + " " * (col - sum_len)
+        result = ""
+        result_len = 0
+        for (char, length) in char_lengths:
+            next_result = result + char
+            next_len = result_len + length
+            if next_len > col - 3:
+                return result + ("...." if result_len < col - 3 else "...")
+            elif next_len == col - 3:
+                return next_result + "..."
+            result = next_result
+            result_len = next_len
+        return ""
+
+    def __char_width(self, char: str, ambiwidth: str) -> int:
+        """
+        this func returns 2 if it is Zenkaku string, 1 if other.  Each type, 'F',
+        'W', 'A', means below.
+
+        * F -- Fullwidth
+        * W -- Wide
+        * A -- Ambiguous
+        """
+        if char not in EAST_ASIAN_WIDTH:
+            EAST_ASIAN_WIDTH[char] = east_asian_width(char)
+        to_double = "FW" if ambiwidth == "single" else "FWA"
+        return 2 if EAST_ASIAN_WIDTH[char] in to_double else 1
--- rplugin/python3/denite/source/memo_grep.py
+++ rplugin/python3/denite/source/memo_grep.py
@@ -4,13 +4,6 @@
 # License: MIT license
 # ============================================================================
 
-import subprocess
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent.parent.resolve()))
-
-from vim_denite_memo.command import Memo, CommandNotFoundError
 from denite.source.grep import Source as Grep
 from denite.util import Nvim, UserContext
 
@@ -21,14 +14,13 @@ class Source(Grep):
 
         self.name = "memo/grep"
 
-    def on_init(self, context: UserContext) -> None:
-        try:
-            memo_dir = Memo().get_memo_dir()
-        except CommandNotFoundError as err:
-            self.error_message(context, str(err))
-        except subprocess.CalledProcessError as err:
-            self.error_message(
-                context, "command returned invalid response: " + str(err)
-            )
-        context["args"].insert(0, memo_dir)
-        super().on_init(context)
+    def on_init(self, context: UserContext):
+        if "memo_dir" not in self.vars or not self.vars["memo_dir"]:
+            self.vars["memo_dir"] = self.vim.call("denite#memo#get_dir")
+        context["args"].insert(0, self.vars["memo_dir"])
+        return super().on_init(context)
+
+    def gather_candidates(self, context: UserContext):
+        if not self.vim.call("denite#memo#executable"):
+            return []
+        return super().gather_candidates(context)
